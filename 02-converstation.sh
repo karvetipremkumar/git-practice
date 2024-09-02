2e643b3d65f215f8b35c88188481e66f69aef2a6
karvetipremkumar <karvetipremkumar@gmail.com>
2024-09-02 15:23:31 +0530

    variables

---
+++ 02-converstation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Mahesh :: Hi Srikanth, How are You?"
+echo "Ramesh :: Hello Mahesh. I am fine. How are you doing?"
+echo "Mahesh :: I am doing good Ramesh. Wahat's going on?"
+echo Ramesh  :: I started learning Shell Script Mahesh"
